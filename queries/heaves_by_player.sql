ce16cc916c06dbdfd11bbd872af21d97a79cc3f2
Jean-Pierre Hurtado <114537712+pierrenero@users.noreply.github.com>
2025-07-19 16:43:20 -0500

    Add files via upload

---
+++ queries/heaves_by_player.sql
@@ -0,0 +1,14 @@
+SELECT
+  Player,
+  SUM(HeavesAtt) AS Total_Heaves_Attempted,
+  SUM(HeavesMd) AS Total_Heaves_Md,
+  ROUND(CAST(SUM(HeavesMd) AS FLOAT) / NULLIF(SUM(HeavesAtt), 0), 3) AS Heave_Pct
+  
+FROM
+  Player_Shooting_Advanced
+  
+GROUP BY
+  Player
+  
+ORDER BY
+  Total_Heaves_Md DESC;
